10549dd99b1e6cd0199273b48936e4389913284c
charles-henri poniard <c.poniard@gmail.com>
2020-04-03 09:36:27 +0200

    add reducers

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
+import channelsReducer from './reducers/channels_reducer';
+import selectedChannelReducer from './reducers/selected_channel_reducer';
+import currentUserReducer from './reducers/current_user_reducer';
+import messagesReducer from './reducers/messages_reducer';
+
 
 // internal modules
 import App from './components/app';
@@ -12,13 +17,16 @@ import '../assets/stylesheets/application.scss';
 const initialState = {
   messages: [],
   currentUser: window.prompt('Your user name please ?') || `anonymous${Math.floor(10 + (Math.random() * 90))}`,
-  channelList: ['Bordeaux', 'Marseille', 'Paris'],
-  channelSelected: 'Bordeaux'
+  channels: ['Bordeaux', 'Marseille', 'Paris'],
+  selectedChannel: 'Bordeaux'
 };
 
 // State and reducers
 const reducers = combineReducers({
-  changeMe: (state = null, action) => state
+  channelsList: channelsReducer,
+  selectChannel: selectedChannelReducer,
+  user: currentUserReducer,
+  messagesList: messagesReducer
 });
 
 // render an instance of the component in the DOM
