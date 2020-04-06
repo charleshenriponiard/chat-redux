7460fdaa44d4589a45b26d94634616f744048905
charles-henri poniard <c.poniard@gmail.com>
2020-04-06 10:33:28 +0200

    color ok

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 
 class Message extends Component {
+
+  hashCode = (str) => { // java String#hashCode
+    var hash = 0;
+    for (var i = 0; i < str.length; i++) {
+       hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    return hash;
+} 
+
+intToRGB = (i) => {
+    let c = (i & 0x00FFFFFF)
+        .toString(16)
+        .toUpperCase();
+    return "00000".substring(0, 6 - c.length) + c;
+}
+
   render() {
     return ( 
       <div>
-        <h3>{this.props.message.author}</h3>
+        <h3 style={{ color: `#${this.intToRGB(this.hashCode(this.props.message.author))}`}} >{this.props.message.author}</h3>
         <p>{this.props.message.content}</p>
       </div>
        
