b12a7148c373eb0d7a6a9e4d003289c0d0abb08f
charles-henri poniard <c.poniard@gmail.com>
2020-04-08 18:23:48 +0200

    fix bug refresh

--- webpack.config.js
+++ webpack.config.js
@@ -2,35 +2,39 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: '!!html-loader!templates/index.html'
-    })
-  ],
-  devtool: 'sourcemap',
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.s?css$/,
-        exclude: /node_modules/,
-        loaders: [ 'style-loader', 'css-loader', 'sass-loader' ]
-      },
-      {
-        test: /\.html$/,
-        loader: 'html-loader'
-      },
-    ]
-  },
-  resolve: {
-    extensions: [ '.js', '.jsx' ]
-  }
+    output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: '!!html-loader!templates/index.html'
+    })
+  ],
+  devtool: 'sourcemap',
+  devServer: {
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.s?css$/,
+        exclude: /node_modules/,
+        loaders: [ 'style-loader', 'css-loader', 'sass-loader' ]
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+    ]
+  },
+  resolve: {
+    extensions: [ '.js', '.jsx' ]
+  }
 };
